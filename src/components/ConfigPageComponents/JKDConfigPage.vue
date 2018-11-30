0c507f80c407eb60cd28edbe4903887a4864534b
layaCoder <412046213@qq.com>
2018-11-30 09:48:39 +0800

    before update jkdConfig

--- src/components/ConfigPageComponents/JKDConfigPage.vue
+++ src/components/ConfigPageComponents/JKDConfigPage.vue
@@ -130,65 +130,76 @@
         </span> -->
       </el-dialog>
     </div>
-    <el-dialog :title="'开关设置'" :visible.sync="dialogSwitchVisible" width="60%" :before-close="handleClose">
-      <el-form :v-model="formSwitchObj" ref="formSwitchObj">
-        <el-row>
-          <el-col :span="8">
-            <span>12-1</span>
-            <el-switch v-model="switch_12_1" active-text="开启" inactive-text="关闭" @change="changeStatus($event)">
-            </el-switch>
-          </el-col>
-          <el-col :span="8">
-            <span class="selectLabel">品牌</span>
-            <el-select v-model="formSwitchObj.ckb_12_1.compId" placeholder="请选择">
-              <el-option v-for="item in optionsComp" :key="item.value" :label="item.label" :value="item.value">
-              </el-option>
-            </el-select>
-          </el-col>
-          <el-col :span="8">
-            <span class="selectLabel">型号</span>
-            <el-select v-model="formSwitchObj.ckb_12_1.equipname" placeholder="请选择">
-              <el-option v-for="item in optionsEquipName" :key="item.value" :label="item.label" :value="item.value">
-              </el-option>
-            </el-select>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="8">
-            <span>12-2</span>
-            <el-switch v-model="switch_12_2" active-text="开启" inactive-text="关闭" @change="changeStatus($event)">
-            </el-switch>
-          </el-col>
-          <el-col :span="8">
-            <span class="selectLabel">品牌</span>
-            <el-select v-model="formSwitchObj.ckb_12_2.compId" placeholder="请选择">
-              <el-option v-for="item in optionsComp" :key="item.value" :label="item.label" :value="item.value">
-              </el-option>
-            </el-select>
-          </el-col>
-          <el-col :span="8">
-            <span class="selectLabel">型号</span>
-            <el-select v-model="formSwitchObj.ckb_12_2.equipname" placeholder="请选择">
-              <el-option v-for="item in optionsEquipName" :key="item.value" :label="item.label" :value="item.value">
-              </el-option>
-            </el-select>
-          </el-col>
-        </el-row>
-      </el-form>
-    </el-dialog>
-    <!-- 开关设置模态框 -->
-    <div>
 
+    <!--开关设置模态框 2 -->
+    <div>
+      <el-dialog :title="'开关设置'" :visible.sync="dialogSwitchVisible" width="60%" :before-close="handleClose">
+       <JKDSwitchComponent :equipCode="this.equipCode"></JKDSwitchComponent>
+      </el-dialog>
     </div>
+
+    <!-- 开关设置模态框 -->
+    <!-- <div>
+      <el-dialog :title="'开关设置'" :visible.sync="dialogSwitchVisible" width="60%" :before-close="handleClose">
+        <el-form :v-model="formSwitchObj" ref="formSwitchObj">
+          <el-row>
+            <el-col :span="8">
+              <span>12-1</span>
+              <el-switch v-model="switch_12_1" active-text="开启" inactive-text="关闭" @change="changeStatus($event)">
+              </el-switch>
+            </el-col>
+            <el-col :span="8">
+              <span class="selectLabel">品牌</span>
+              <el-select v-model="formSwitchObj.ckb_12_1.compId" placeholder="请选择">
+                <el-option v-for="item in optionsComp" :key="item.value" :label="item.label" :value="item.value">
+                </el-option>
+              </el-select>
+            </el-col>
+            <el-col :span="8">
+              <span class="selectLabel">型号</span>
+              <el-select v-model="formSwitchObj.ckb_12_1.equipname" placeholder="请选择">
+                <el-option v-for="item in optionsEquipName" :key="item.value" :label="item.label" :value="item.value">
+                </el-option>
+              </el-select>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="8">
+              <span>12-2</span>
+              <el-switch v-model="switch_12_2" active-text="开启" inactive-text="关闭" @change="changeStatus($event)">
+              </el-switch>
+            </el-col>
+            <el-col :span="8">
+              <span class="selectLabel">品牌</span>
+              <el-select v-model="formSwitchObj.ckb_12_2.compId" placeholder="请选择">
+                <el-option v-for="item in optionsComp" :key="item.value" :label="item.label" :value="item.value">
+                </el-option>
+              </el-select>
+            </el-col>
+            <el-col :span="8">
+              <span class="selectLabel">型号</span>
+              <el-select v-model="formSwitchObj.ckb_12_2.equipname" placeholder="请选择">
+                <el-option v-for="item in optionsEquipName" :key="item.value" :label="item.label" :value="item.value">
+                </el-option>
+              </el-select>
+            </el-col>
+          </el-row>
+        </el-form>
+      </el-dialog>
+    </div> -->
   </div>
 </template>
 
 <script>
 import API from "../../apis/index.js";
+import JKDSwitchComponent from '../parts/ConfigComponents/JKDSwitchComponent.vue'
 
 export default {
+  components:{JKDSwitchComponent},
   data() {
+    
     return {
+      equipCode:'', //设备编号，用props传递给子组件
       formType: 0, //表单状态，0位新增，1为修改
       formObj: {
         WS_Area_Code: "",
@@ -204,75 +215,12 @@ export default {
         WS_Org_Code: "",
         WS_SysCode: ""
       },
-      switch_12_1: false,
-      switch_12_2: false,
-      formSwitchObj: {
-        ckb_12_1: {
-          active: "",
-          compId: "",
-          equipname: ""
-        },
-        ckb_12_2: {
-          active: false,
-          compId: "",
-          equipname: ""
-        },
-        ckb_12_3: {
-          active: false,
-          compId: "",
-          equipname: ""
-        },
-        ckb_12_4: {
-          active: false,
-          compId: "",
-          equipname: ""
-        },
-        ckb_24_1: {
-          active: false,
-          compId: "",
-          equipname: ""
-        },
-        ckb_24_2: {
-          active: false,
-          compId: "",
-          equipname: ""
-        },
-        ckb_220_1: {
-          active: false,
-          compId: "",
-          equipname: ""
-        },
-        ckb_220_2: {
-          active: false,
-          compId: "",
-          equipname: ""
-        },
-        ckb_poe_1: {
-          active: false,
-          compId: "",
-          equipname: ""
-        },
-        ckb_poe_2: {
-          active: false,
-          compId: "",
-          equipname: ""
-        },
-        ckb_poe_3: {
-          active: false,
-          compId: "",
-          equipname: ""
-        },
-        ckb_poe_4: {
-          active: false,
-          compId: "",
-          equipname: ""
-        }
-      },
+      
       tableData: [
         // {
         //   ID: "01",
-        //   ComName: "RTST",
-        //   Operator: "liuchunfu",
+        //   ComName: "skt1",
+        //   Operator: "laya",
         //   Address: "武大园二路",
         //   Tel: "110"
         // }
@@ -440,6 +388,7 @@ export default {
     },
     handleShowSwitchModal: function(index, row) {
       console.log(row);
+      this.equipCode= row.WS_Code
       this.dialogSwitchVisible = true;
     },
     changeStatus: function(e) {
---
+++ src/components/parts/ConfigComponents/JKDSwitchComponent.vue
@@ -0,0 +1,95 @@
+<template>
+   <div >
+     <div>switch components</div>
+     <div>{{equipCode}}</div>
+   </div>
+</template>
+
+<script>
+ export default {
+     props: {
+    equipCode: {
+      type: String,
+      required: true
+    }
+  },
+    data() {
+      return {
+       switch_12_1: false,
+      switch_12_2: false,
+      formSwitchObj: {
+        ckb_12_1: {
+          active: "",
+          compId: "",
+          equipname: ""
+        },
+        ckb_12_2: {
+          active: false,
+          compId: "",
+          equipname: ""
+        },
+        ckb_12_3: {
+          active: false,
+          compId: "",
+          equipname: ""
+        },
+        ckb_12_4: {
+          active: false,
+          compId: "",
+          equipname: ""
+        },
+        ckb_24_1: {
+          active: false,
+          compId: "",
+          equipname: ""
+        },
+        ckb_24_2: {
+          active: false,
+          compId: "",
+          equipname: ""
+        },
+        ckb_220_1: {
+          active: false,
+          compId: "",
+          equipname: ""
+        },
+        ckb_220_2: {
+          active: false,
+          compId: "",
+          equipname: ""
+        },
+        ckb_poe_1: {
+          active: false,
+          compId: "",
+          equipname: ""
+        },
+        ckb_poe_2: {
+          active: false,
+          compId: "",
+          equipname: ""
+        },
+        ckb_poe_3: {
+          active: false,
+          compId: "",
+          equipname: ""
+        },
+        ckb_poe_4: {
+          active: false,
+          compId: "",
+          equipname: ""
+        }
+      },
+      };
+    },
+    methods: {
+      
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+
+
+</style>
